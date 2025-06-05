c286ea83d4e03c459aa9ebcc31c28a99d140cd92
kylemoorehead-pingidentity <117233001+kylemoorehead-pingidentity@users.noreply.github.com>
2025-06-05 14:14:11 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "pingone" {
   client_secret     = var.worker_secret
   environment_id    = var.pingone_environment_id
   region_code       = var.region_code
-  append_user_agent = "PingOne-B2B/1.0.0"
+  append_user_agent = "PingOne-B2B/1.0.3"
 }
 
 provider "davinci" {
